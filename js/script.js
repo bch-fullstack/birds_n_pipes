1891acdb7f8ea78455e6a495afbfc98e859ca624
Squirtle <tienhoang.pham@bc.fi>
2020-09-18 10:45:22 +0300

    update to use Class notation

--- js/script.js
+++ js/script.js
@@ -1,98 +1,76 @@
-var infiniteBackground = {
-    el: null,
-    style: {
-        left: 0,
-        top: 0,
-        position: 'fixed',
-        height: '100%',
-        width: '100%',
-        backgroundImage: null,
-        backgroundRepeat: 'repeat-x',
-        backgroundPosition: '0 0',
-        backgroundSize: 'auto 100%'
-    },
-    scrollSideWay: function(){
-        var scope = this;
-        document.addEventListener('scroll', function(){
-            var topOffset = $(scope.el).offset().top;
-
-            scope.el.style.backgroundPosition = `-${topOffset}px 0px`
-        })
-    },
-    init: function(el, bg = '../img/bg.png'){
+class Background {
+    constructor(el, bg = '../img/bg.png'){
         this.el = el;
-        this.style.backgroundImage = `url("${bg}")`;
+        this.style = {
+            left: 0,
+            top: 0,
+            position: 'fixed',
+            height: '100%',
+            width: '100%',
+            backgroundImage: null,
+            backgroundRepeat: 'repeat-x',
+            backgroundPosition: '0 0',
+            backgroundSize: 'auto 100%',
+            backgroundImage: `url("${bg}")`
+        };
+        this.moveRatio = 50;
         Object.assign(this.el.style, this.style);
-        this.scrollSideWay();
+    }
+
+    scrollSideWay(distance){
+        this.el.style.backgroundPosition = `-${distance / this.moveRatio}px 0px`;
     }
 }
 
-var pipe = { // definition of object of the class pipe
-    id: null,
-    imgSrc: '../img/pipe.png',
-    size: null,
-    style: {
-        position: 'fixed',
-        top: null,
-        left: null,
-        width: null,
-        bottom: 0
-    },
-    flip: false,
-    // public method
-    moveLeft: function(distance){
-        var _current = parseInt(document.getElementById(this.id).style.left)
-        document.getElementById(this.id).style.left = `${_current - distance/20}px`;  
-    },
-    init: function(parentEl){
-        var scope = this;
+class Pipe {
+    constructor(parentEl){
         this.id = 'pipe_' + Math.floor(Math.random() * 2000);
+        this.imgSrc = '../img/pipe.png';
+        this.size = null;
+        this.style = {
+            position: 'fixed',
+            top: null,
+            left: Math.floor(Math.random() * 2000) + 'px',
+            width: Math.floor(Math.random() * 200) + 'px',
+            bottom: 0
+        };
+        this.flip = false;
+        this.moveRatio = 20;
 
         var pipeEl = document.createElement('img');
         pipeEl.src = this.imgSrc;
         pipeEl.id = this.id;
-
-        this.style.left = Math.floor(Math.random() * 2000) + 'px';
-        this.style.width = Math.floor(Math.random() * 200) + 'px';
-
-        // extends pipeEl.style with properties of this.style
         Object.assign(pipeEl.style, this.style); 
+
         parentEl.appendChild(pipeEl);
+    }
+
+    moveLeft(distance){
+        document.getElementById(this.id).style.left = `${parseInt(this.style.left) - distance / this.moveRatio}px`;  
     }
 }
 
 $(document).ready(function(){
-    var background = Object.create(infiniteBackground);
-
-    background.init(
+    var background = new Background(
         document.getElementById('background')
     );
 
-    var pipe1 = Object.create(pipe);
-    var pipe2 = Object.create(pipe);
-    var pipe3 = Object.create(pipe);
-    var pipe4 = Object.create(pipe);
-    var pipe5 = Object.create(pipe);
+    document.addEventListener('scroll', function(){
+        var offset = window.scrollY;
 
-    pipe1.init(document.body);
-    pipe2.init(document.body);
-    pipe3.init(document.body);
-    pipe4.init(document.body);
-    pipe5.init(document.body);
-
-    $(document).on('scroll', function(){
-        var topOffset = $(window).scrollTop();
-        pipe1.moveLeft(topOffset)
-        pipe2.moveLeft(topOffset)
-        pipe3.moveLeft(topOffset)
-        pipe4.moveLeft(topOffset)
-        pipe5.moveLeft(topOffset)
+        background.scrollSideWay(offset);
+        pipe1.moveLeft(offset)
+        pipe2.moveLeft(offset)
+        pipe3.moveLeft(offset)
+        pipe4.moveLeft(offset)
+        pipe5.moveLeft(offset)
     })
 
-    // $(document).on('scroll', function(e){
-    //     var topOffset = $('#overlay').offset().top;
-    //     $('#overlay').css('background-position', `-${topOffset}px 0px`)
-    // })
+    var pipe1 = new Pipe(document.body);
+    var pipe2 = new Pipe(document.body);
+    var pipe3 = new Pipe(document.body);
+    var pipe4 = new Pipe(document.body);
+    var pipe5 = new Pipe(document.body);
 });
 
 
