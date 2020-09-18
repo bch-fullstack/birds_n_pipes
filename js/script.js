9b04e2a8f98ddd6ef45af68b742a5e30126358f4
Squirtle <tienhoang.pham@bc.fi>
2020-09-18 10:23:15 +0300

    Add broken code

--- js/script.js
+++ js/script.js
@@ -28,6 +28,7 @@ var infiniteBackground = {
 }
 
 var pipe = { // definition of object of the class pipe
+    id: null,
     imgSrc: '../img/pipe.png',
     size: null,
     style: {
@@ -38,30 +39,71 @@ var pipe = { // definition of object of the class pipe
         bottom: 0
     },
     flip: false,
+    // public method
+    moveLeft: function(distance){
+        var _current = parseInt(document.getElementById(this.id).style.left)
+        document.getElementById(this.id).style.left = `${_current - distance/20}px`;  
+    },
     init: function(parentEl){
-        var pipeEl = document.createElement('img');
-        
-        pipeEl.src = this.imgSrc;
-        this.style.left = Math.floor(Math.random() * 1000) + 'px';
-        this.style.width = Math.floor(Math.random() * 200) + 'px';
-        Object.assign(pipeEl.style, this.style)
+        var scope = this;
+        this.id = 'pipe_' + Math.floor(Math.random() * 2000);
 
+        var pipeEl = document.createElement('img');
+        pipeEl.src = this.imgSrc;
+        pipeEl.id = this.id;
+
+        this.style.left = Math.floor(Math.random() * 2000) + 'px';
+        this.style.width = Math.floor(Math.random() * 200) + 'px';
+
+        // extends pipeEl.style with properties of this.style
+        Object.assign(pipeEl.style, this.style); 
         parentEl.appendChild(pipeEl);
     }
 }
 
 $(document).ready(function(){
-    infiniteBackground.init(
+    var background = Object.create(infiniteBackground);
+
+    background.init(
         document.getElementById('background')
     );
 
-    var pipe1 = pipe;
-    var pipe2 = pipe;
+    var pipe1 = Object.create(pipe);
+    var pipe2 = Object.create(pipe);
+    var pipe3 = Object.create(pipe);
+    var pipe4 = Object.create(pipe);
+    var pipe5 = Object.create(pipe);
+
     pipe1.init(document.body);
     pipe2.init(document.body);
+    pipe3.init(document.body);
+    pipe4.init(document.body);
+    pipe5.init(document.body);
+
+    $(document).on('scroll', function(){
+        var topOffset = $(window).scrollTop();
+        pipe1.moveLeft(topOffset)
+        pipe2.moveLeft(topOffset)
+        pipe3.moveLeft(topOffset)
+        pipe4.moveLeft(topOffset)
+        pipe5.moveLeft(topOffset)
+    })
 
     // $(document).on('scroll', function(e){
     //     var topOffset = $('#overlay').offset().top;
     //     $('#overlay').css('background-position', `-${topOffset}px 0px`)
     // })
 });
+
+
+var fox = {
+    hair: 'orange',
+    tail: true,
+    growl: function(){
+        console.log('meow')
+    }
+}
+
+console.log(fox.hair)
+fox.growl()
+
